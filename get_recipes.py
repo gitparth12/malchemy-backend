55e91c8f33920d691c109f14155065a4a267fae8
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-03-16 11:15:52 +1100

    Add code to get recipes from spoonacular

--- get_recipes.py
+++ get_recipes.py
@@ -1,12 +1,30 @@
 import requests
+import json
 from os import environ
 
 RECIPE_SEARCH_URL = "https://api.spoonacular.com/recipes/complexSearch"
 RECIPE_INFO_URL = "https://api.spoonacular.com/recipes/informationBulk"
 
+SPOONACULAR_API_KEY = environ.get('SPOONACULAR_API_KEY')
+GEMINI_API_KEY = environ.get('GEMINI_API_KEY')
+
 if not environ.get('SPOONACULAR_API_KEY') or not environ.get('GEMINI_API_KEY'):
     print("Can't access at least one necessary API key, exiting.")
     exit(1)
 
+params = {
+    "includeIngredients": "chicken,tomato,basil,garlic,onion,carrot,broccoli,\
+            mushroom,cheese,eggs,rice,pasta,beans,spinach,cilantro,lemon,avocado",
+    "cuisine": "italian,mexican",
+    "intolerances": "shellfish,peanuts",
+    "number": 10,
+    "sort": "meta-score",
+    "apiKey": SPOONACULAR_API_KEY,
+}
 
+response = requests.get(RECIPE_SEARCH_URL, params=params)
 
+if response.status_code == 200:
+    print(json.dumps(response.json(), indent=2))
+else:
+    print("Error:", response.status_code, response.text)
