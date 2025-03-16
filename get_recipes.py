52d40387ff37fc0934ff5db59b629fb8ff6419f6
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-03-16 11:13:14 +1100

    Remove helpers file, move url constants to main file

--- get_recipes.py
+++ get_recipes.py
@@ -1,6 +1,9 @@
 import requests
 from os import environ
 
+RECIPE_SEARCH_URL = "https://api.spoonacular.com/recipes/complexSearch"
+RECIPE_INFO_URL = "https://api.spoonacular.com/recipes/informationBulk"
+
 if not environ.get('SPOONACULAR_API_KEY') or not environ.get('GEMINI_API_KEY'):
     print("Can't access at least one necessary API key, exiting.")
     exit(1)
